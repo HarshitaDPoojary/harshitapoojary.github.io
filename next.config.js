a50b23bd906ea60cce3e988ce2042c5e15fe28bd
Harshita Poojary <dmpoojary6458@gmail.com>
2025-09-17 23:48:26 -0700

    added console logs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 // next.config.js
 const isProd = process.env.NODE_ENV === "production";
-
+console.log(">>> NODE_ENV:", process.env.NODE_ENV, "isProd:", isProd);
+console.log(">>> basePath:", isProd ? "/harshitapoojary.github.io" : "(none)");
 module.exports = {
   output: "export",
   images: { unoptimized: true },
