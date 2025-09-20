201c6dec7ba36f44caaf0f1a00dc2231460c34a6
Harshita Poojary <dmpoojary6458@gmail.com>
2025-09-19 17:56:59 -0700

    REduce  size of main heading in HeroSection because Developer was going in next line

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -19,18 +19,29 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              Hello, I&apos;m{" "}
-            </span>
-            <br />
+          <h1 className="text-white mb-4 text-2xl sm:text-3xl lg:text-5xl lg:leading-normal font-extrabold">
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600 text-xl sm:text-6xl lg:text-7xl">
+            Hello, I&apos;m{" "}
+          </span>
+          <br />
+          <span className="text-xl sm:text-6xl lg:text-7xl font-bold">
             <TypeAnimation
-              sequence={["Harshita Poojary", 1000, "a Software Developer", 1000, "a Data Scientist", 1000, "a Tech Enthusiast", 1000]}
+              sequence={[
+                "Harshita Poojary",
+                1000,
+                "a Software Developer",
+                1000,
+                "a Data Scientist",
+                1000,
+                "a Tech Enthusiast",
+                1000,
+              ]}
               wrapper="span"
               speed={50}
               repeat={Infinity}
             />
-          </h1>
+          </span>
+        </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             Welcome to my portfolio website!
           </p>
